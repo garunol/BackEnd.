a433532ea0e8d82469720b80cfb3b58194bdd380
garunol <162654693+garunol@users.noreply.github.com>
2025-05-14 16:07:01 +0700

    Add files via upload

---
+++ LAB2/Controllers/TodoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+public class TodoController : Controller
+{
+    private static List<TodoModel> _todos = new List<TodoModel>();
+
+    public IActionResult Index()
+    {
+        return View(_todos);
+    }
+
+    // GET: Todo/Add
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    // POST: Todo/Add
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Add(TodoModel todo)
+    {
+        if (ModelState.IsValid)
+        {
+            // Gán ID tự động
+            todo.Id = _todos.Count > 0 ? _todos.Max(t => t.Id) + 1 : 1;
+            _todos.Add(todo);
+            return RedirectToAction("Index");
+        }
+
+        return View(todo);
+    }
+
+    // GET: Todo/Edit/5
+    public IActionResult Edit(int id)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo == null)
+            return NotFound();
+
+        return View(todo);
+    }
+
+    // POST: Todo/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(TodoModel todo)
+    {
+        if (ModelState.IsValid)
+        {
+            var existing = _todos.FirstOrDefault(t => t.Id == todo.Id);
+            if (existing == null)
+                return NotFound();
+
+            existing.Title = todo.Title;
+            existing.Description = todo.Description;
+            return RedirectToAction("Index");
+        }
+
+        return View(todo);
+    }
+}
---
+++ LAB2/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
